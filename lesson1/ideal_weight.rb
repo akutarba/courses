46a4854282a56973ecd01ab73b4880a3c699095d
Anna Kutarba <anna.kutarba@jetbrains.com>
2018-01-26 19:09:12 +0100

    lesson1: ideal_weight and triangle_square without classes

--- lesson1/ideal_weight.rb
+++ lesson1/ideal_weight.rb
@@ -1,16 +1,5 @@
 # calculating ideal weight according hight
 
-
-class IdealWeight
-  def calc_ideal_weight(current_hight)
-    if (current_hight - 110) > 0
-      print "Your ideal weight is #{current_hight - 110}"
-    else
-      print "You are already perfect!"
-    end
-  end
-end
-
 print "Welcome to the program for calculating your ideal weight. \n"
 
 print "What is your first name? "
@@ -24,4 +13,10 @@ current_hight = gets.chomp.to_i
 
 print "Hi #{first_name} #{last_name}. "
 
-IdealWeight.new.calc_ideal_weight(current_hight)
+if (current_hight - 110) > 0
+  print "Your ideal weight is #{current_hight - 110}"
+else
+  print "You are already perfect!"
+end
+
+
